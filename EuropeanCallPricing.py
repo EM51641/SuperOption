1ab25e336ef6e2fe6967fd18a0718e7ed95dd7a9
Elyes Mahjoubi <65829912+EM51641@users.noreply.github.com>
2020-07-24 21:37:34 +0100

    Create EuropeanCallPricing.py

---
+++ EuropeanCallPricing.py
@@ -0,0 +1,40 @@
+class EuropeanCall:
+
+    def call_delta(
+        self, asset_price, asset_volatility, strike_price,
+        time_to_expiration, risk_free_rate,q
+            ):
+        b = math.exp(-risk_free_rate*time_to_expiration)
+        x1 = math.log(asset_price/(strike_price)) + .5*(asset_volatility*asset_volatility+risk_free_rate-q)*time_to_expiration
+        x1 = x1/(asset_volatility*(time_to_expiration**.5))
+        z1 = norm.cdf(x1)
+        return z1
+
+    def call_price(
+        self, asset_price, asset_volatility, strike_price,
+        time_to_expiration, risk_free_rate,q
+            ):
+        b  = math.exp(-risk_free_rate*time_to_expiration)
+        x1 = math.log(asset_price/(strike_price))+(.5*(asset_volatility**2)+risk_free_rate-q)*time_to_expiration
+        x1 = x1/(asset_volatility*(time_to_expiration**.5))
+        z1 = norm.cdf(x1)
+        z1 = z1*asset_price*math.exp(-q*time_to_expiration)
+        x2 = math.log(asset_price/(strike_price)) - (.5*(asset_volatility**2)-risk_free_rate+q)*time_to_expiration
+        x2 = x2/(asset_volatility*(time_to_expiration**.5))
+        z2 = norm.cdf(x2)
+        z2 = b*strike_price*z2
+        return z1 - z2
+
+    def __init__(
+        self, asset_price, asset_volatility, strike_price,
+        time_to_expiration, risk_free_rate,q
+            ):
+        self.asset_price = asset_price
+        self.asset_volatility = asset_volatility
+        self.strike_price = strike_price
+        self.time_to_expiration = time_to_expiration
+        self.risk_free_rate = risk_free_rate
+        self.q = q
+        self.price = self.call_price(asset_price, asset_volatility, strike_price, time_to_expiration, risk_free_rate,q)
+        self.delta = self.call_delta(asset_price, asset_volatility, strike_price, time_to_expiration, risk_free_rate,q)
+        
